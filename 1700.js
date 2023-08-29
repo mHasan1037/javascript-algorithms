8e72f092be81d60dbe96c1f4a11c0d3e4fbaf20d
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-08-29 06:12:39 +0600

    solve 1700th leetcode problem

---
+++ 1700.js
@@ -0,0 +1,40 @@
+/*
+The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+
+The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
+
+If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and leave the queue.
+Otherwise, they will leave it and go to the queue's end.
+This continues until none of the queue students want to take the top sandwich and are thus unable to eat.
+
+You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
+*/
+
+
+var countStudents = function(students, sandwiches) {
+    let stud = students
+    let sand = sandwiches
+
+
+    if(stud[0] === sand[0]){
+        stud.shift()
+        sand.shift()
+
+        if (stud.length === 0 && sand.length === 0) {
+            return 0;
+        }
+
+        return countStudents(stud, sand)
+    }else{
+       let firstStud = stud[0]
+       stud.shift()
+       stud.push(firstStud)
+
+       if (stud.every((s)=> s == 1) || stud.every((s)=> s == 0)) {
+        return stud.length;
+        }
+        return countStudents(stud, sand)
+    }
+};
+
+countStudents([1,1,1,0,0,1], [1,0,0,0,1,1])
